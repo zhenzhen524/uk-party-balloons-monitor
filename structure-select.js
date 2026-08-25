72f50195237146430397db2e08d7e973465f3815
zhenzhen524 <liuzhentot@gmail.com>
2026-08-25 10:41:18 +0800

    Load parent sales display and market panorama scripts

--- structure-select.js
+++ structure-select.js
@@ -1,3 +1,4 @@
-document.write('<script src="structure-select-base.js?v=20260825-filter6"><\/script>');
-document.write('<script src="threshold-rule.js?v=20260825-filter6"><\/script>');
-document.write('<script src="parent-sales-display.js?v=20260825-parent-sales1"><\/script>');
+document.write('<script src="structure-select-base.js?v=20260825-filter7"><\/script>');
+document.write('<script src="threshold-rule.js?v=20260825-filter7"><\/script>');
+document.write('<script src="parent-sales-display.js?v=20260825-parent-sales2"><\/script>');
+document.write('<script src="market-panorama.js?v=20260825-panorama1"><\/script>');
