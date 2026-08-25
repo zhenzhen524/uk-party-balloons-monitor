f71a288ffe658d1018cba9c73073de3e5dc30392
zhenzhen524 <liuzhentot@gmail.com>
2026-08-25 10:24:30 +0800

    Load parent sales display correction

--- structure-select.js
+++ structure-select.js
@@ -1,2 +1,3 @@
-document.write('<script src="structure-select-base.js?v=20260825-filter5"><\/script>');
-document.write('<script src="threshold-rule.js?v=20260825-filter5"><\/script>');
+document.write('<script src="structure-select-base.js?v=20260825-filter6"><\/script>');
+document.write('<script src="threshold-rule.js?v=20260825-filter6"><\/script>');
+document.write('<script src="parent-sales-display.js?v=20260825-parent-sales1"><\/script>');
