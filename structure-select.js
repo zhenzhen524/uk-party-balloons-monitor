6c35dfd073cd1ead3608d0191e33e8eb009488e5
zhenzhen524 <liuzhentot@gmail.com>
2026-08-25 09:57:29 +0800

    Bump filter rule cache version

--- structure-select.js
+++ structure-select.js
@@ -1,2 +1,2 @@
-document.write('<script src="structure-select-base.js?v=20260824-filter4"><\/script>');
-document.write('<script src="threshold-rule.js?v=20260824-filter4"><\/script>');
+document.write('<script src="structure-select-base.js?v=20260825-filter5"><\/script>');
+document.write('<script src="threshold-rule.js?v=20260825-filter5"><\/script>');
