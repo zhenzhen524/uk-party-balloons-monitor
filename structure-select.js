98d4ac5bd7cfe8bacb1a02c0dd48ad13e042aa28
zhenzhen524 <liuzhentot@gmail.com>
2026-08-26 10:18:36 +0800

    Bump history trend center cache version

--- structure-select.js
+++ structure-select.js
@@ -2,4 +2,4 @@ document.write('<script src="structure-select-base.js?v=20260826-history1"><\/sc
 document.write('<script src="threshold-rule.js?v=20260826-history1"><\/script>');
 document.write('<script src="parent-sales-display.js?v=20260826-parent-sales3"><\/script>');
 document.write('<script src="market-panorama.js?v=20260826-panorama2"><\/script>');
-document.write('<script src="history-brand-ui.js?v=20260826-history-brand1"><\/script>');
+document.write('<script src="history-brand-ui.js?v=20260826-history-brand2"><\/script>');
