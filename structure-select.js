f79a412ddbc2bb2db4588fb87af8e95060f829e7
zhenzhen524 <liuzhentot@gmail.com>
2026-08-26 10:08:21 +0800

    Load history viewer and brand highlight UI

--- structure-select.js
+++ structure-select.js
@@ -1,4 +1,5 @@
-document.write('<script src="structure-select-base.js?v=20260825-filter7"><\/script>');
-document.write('<script src="threshold-rule.js?v=20260825-filter7"><\/script>');
-document.write('<script src="parent-sales-display.js?v=20260825-parent-sales2"><\/script>');
-document.write('<script src="market-panorama.js?v=20260825-panorama1"><\/script>');
+document.write('<script src="structure-select-base.js?v=20260826-history1"><\/script>');
+document.write('<script src="threshold-rule.js?v=20260826-history1"><\/script>');
+document.write('<script src="parent-sales-display.js?v=20260826-parent-sales3"><\/script>');
+document.write('<script src="market-panorama.js?v=20260826-panorama2"><\/script>');
+document.write('<script src="history-brand-ui.js?v=20260826-history-brand1"><\/script>');
